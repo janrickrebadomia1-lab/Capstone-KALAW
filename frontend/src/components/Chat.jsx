ed29780d091c7fb51e89dbf1a0439cf77f274b85
Jan Rick <janrickrebadomia1@gmail.com>
2026-08-03 20:42:52 -0700

    Fix KALAW backend API connection

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -258,7 +258,7 @@ const Chat = forwardRef(({ sidebarInput, setSidebarInput, setChatHistory, setAct
 
 try {
   const response = await fetch(
-  "https://speech-powered-pets-metric.trycloudflare.com  /api/chat",
+  "https://discovery-refers-findings-queen.trycloudflare.com/api/chat",
   {
     method: "POST",
     headers: {
